dc76697dbe772ac56dc316a075e46deaeb65abda
MaxVin <maxvin2002vinmax@gmail.com>
2023-05-15 17:02:46 -0700

    First commit

---
+++ my-app/src/AlgorithmVisualizer/AlgorithmVisualizer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./AlgorithmVisualizer.css";
+
+export default function AlgorithmVisualizer () {
+
+
+
+    return(
+        <div></div>
+    );
+}
---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
