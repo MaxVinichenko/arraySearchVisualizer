7becb47143d11938c87bcf3d9937c573a46d3ab8
Max <maxvin2002vinmax@gmail.com>
2024-05-12 20:38:53 -0700

    added router basename

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,7 +10,7 @@ import PathfindingPage from './pages/PathfindingPage';
 export default function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/arrayVisualizer" element={<ArrayPage />} />
