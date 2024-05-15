dc0a57b1b91e960d9ae3325a049455bf5780e845
MaxVin <maxvin2002vinmax@gmail.com>
2024-05-14 23:54:58 -0700

    Path_vis nodes rendering

--- my-app/src/Pathfinding_visualizer/Path_vis.jsx
+++ my-app/src/Pathfinding_visualizer/Path_vis.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useRef, useEffect } from "react";
-
+import Node from "./Node/Node.jsx"
 
 export default function PathgingVisualizer(){
-//Mounting nodes
+
+  const [grid, setGrid] = useState([]);
+
+  //Mounting grid
   useEffect(() => {
     handleSetGrid();
   }, []);
 
-    //Create nodes
+    //Create grid
   function handleSetGrid(){
     let xlen = 10;
     let ylen = 10;
@@ -15,22 +18,38 @@ export default function PathgingVisualizer(){
 
     // The gird will be a 2D array of objects x is i1 and y is i2
     // so it'll be [x][y] starting at [0][0]
-    i1=0
-    i2=0
+    let i1=0
+    let i2=0
     while(xlen>i1){
-      while(height>0){
+      while(ylen>i2){
         newGrid.push({
           x: i1,
           y: i2,
           isVisited: false,
-          distance: xlen*ylen;
+          distance: xlen*ylen
         });
-        }
         i2+=1;
       }
       i1+=1;
     }
-
-
+    setGrid([...newGrid]);
   }
+
+  return (
+    <>
+      {
+        grid.map((node) => {
+          node.map((index) => {
+            return (
+              <Node node={node} key={index} />
+            );
+          });
+        })
+      }
+
+    </>
+  );
+
+
 }
+
