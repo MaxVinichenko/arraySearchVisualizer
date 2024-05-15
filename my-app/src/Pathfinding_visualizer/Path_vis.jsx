953986683480be8b52197c597faf5fc71691b10a
Max <maxvin2002vinmax@gmail.com>
2024-05-15 12:37:24 -0700

    Pathfinding grid added

--- my-app/src/Pathfinding_visualizer/Path_vis.jsx
+++ my-app/src/Pathfinding_visualizer/Path_vis.jsx
@@ -18,36 +18,43 @@ export default function PathgingVisualizer(){
 
     // The gird will be a 2D array of objects x is i1 and y is i2
     // so it'll be [x][y] starting at [0][0]
-    let i1=0
-    let i2=0
-    while(xlen>i1){
-      while(ylen>i2){
-        newGrid.push({
+
+    for(let i1=0; i1<xlen; i1++){
+      let col=[];
+      for(let i2=0; i2<ylen; i2++){
+        col.push({
           x: i1,
           y: i2,
           isVisited: false,
           distance: xlen*ylen
         });
-        i2+=1;
       }
-      i1+=1;
+      newGrid.push(col);
     }
     setGrid([...newGrid]);
   }
 
   return (
-    <>
+    <div className="container">
+      
+
       {
-        grid.map((node) => {
-          node.map((index) => {
-            return (
-              <Node node={node} key={index} />
-            );
-          });
+        grid.map((col) => {
+          return (
+            <div className="column">{ 
+              col.map((node, index) => {
+                return (
+                  <Node node={node} key={index} />
+                );
+              })
+             }</div>
+          );
         })
       }
 
-    </>
+
+
+    </div>
   );
 
 
